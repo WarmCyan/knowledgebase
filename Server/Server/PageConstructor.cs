ac576d0731d5bbe42b1aec9536eeab14d3ea7e24
Nathan Martindale (WildfireXIII) <nmblenderdude0@gmail.com>
2016-10-11 23:04:39 -0500

    Progress on page construction

--- Server/Server/PageConstructor.cs
+++ Server/Server/PageConstructor.cs
@@ -3,31 +3,76 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace KnowledgeBaseServer
 {
 	public class PageConstructor
 	{
-		private static List<string> s_lTags;
-	
+		private static List<string> s_lQueryTags;
+
+		private static List<string> s_lGenericTags = new List<string>() { "Definition", "Theory", "Argument", "Wisdom", "Note", "Depth" };
+
+		private static string s_sInterestTag = "";
+
 		// return page HTML
 		public static string Construct(string sOriginalQuery, List<Snippet> lSnippets)
 		{
-			s_lTags = new List<string>();
+			s_lQueryTags = new List<string>();
 		
+			// title/query list
 			string sHTML = "<body>";
 			sHTML += TitleHtml(sOriginalQuery);
 			sHTML += "<div class='separator'></div>";
 
+			// find tag of interest
+			DetermineInterestTag();
+
+			// find definition or theory related directly to tag of interest
+			sHTML += InterestTagHtml(lSnippets);
+
+			sHTML += "</body>";
 			return sHTML;
+		}
+
+		private static string InterestTagHtml(List<Snippet> lSnippets)
+		{
+			string sHTML = "";
+			// find a snippet with a tag of InterestTag_Definition or InterestTag_Theory
+			foreach (Snippet pSnippet in lSnippets)
+			{
+				if (pSnippet.Tags.Contains(s_sInterestTag + "_Definition") || pSnippet.Tags.Contains(s_sInterestTag + "_Theory"))
+				{
+					sHTML += "<div class='snippet primary'>";
+
+					// get the body of the snippet (ignore source for now)
+					XElement pXML = XElement.Parse(pSnippet.Content);
+					sHTML += pXML.Element("snippet").Value;
+				
+					sHTML += "</div>";
+				}
+			}
+			return sHTML;
+		}
+
+		private static void DetermineInterestTag()
+		{
+			// go backwards and find first non generic tag
+			for (int i = s_lQueryTags.Count - 1; i >= 0; i--)
+			{
+				string sTag = s_lQueryTags[i];
+				if (s_lGenericTags.Contains(sTag)) { continue; }
+				s_sInterestTag = sTag;
+				return;
+			}
 		}
 
 		private static string TitleHtml(string sOriginalQuery)
 		{
-			s_lTags = sOriginalQuery.Split(',').ToList();
+			s_lQueryTags = sOriginalQuery.Split(',').ToList();
 
 			string sTagString = "";
-			foreach (string sTag in s_lTags) { sTagString += sTag.Replace('_', ' ') + " "; }
+			foreach (string sTag in s_lQueryTags) { sTagString += sTag.Replace('_', ' ') + " "; }
 			sTagString = sTagString.Trim();
 
 			string sHTML = "<div class='snippet title'><h1>" + sTagString + "</h1></div>";
--- Server/Server/Program.cs
+++ Server/Server/Program.cs
@@ -11,7 +11,7 @@ namespace KnowledgeBaseServer
 	{
 		public static void Main(string[] args)
 		{
-			Console.Write("Hello world!");
+			Console.WriteLine("Hello world!");
 
 			WebCommunications.AuthKey = "54003c32a190b6063fe06a528bc230ce151b589512db4a39ecf8ac01be393dafa154f39bde1f56e690f4c3c2870323972240d4d02fc4fa2f3349dc7ef4c7dc09";
 
@@ -35,9 +35,15 @@ namespace KnowledgeBaseServer
 			//string sResponse = WebCommunications.SendPostRequest("http://dwlapi.azurewebsites.net/api/reflection/KnowledgeBaseServer/KnowledgeBaseServer/KnowledgeServer/AddSnippet", sBody, true);
 
 			//AddTestingSnippets();
+
+			string sPage = Query("Genetic_Algorithm");
+			
 			Console.WriteLine("Finished");
+			Console.WriteLine("\n" + sPage);
 			Console.Read();
 		}
+
+		// TODO: have to make snippet format as actual html, (meta tag inside for source) so don't parse as xml later.
 
 		public static void AddSnippet(string sTagList, string sSnippet)
 		{
@@ -45,16 +51,25 @@ namespace KnowledgeBaseServer
 			WebCommunications.SendPostRequest("http://dwlapi.azurewebsites.net/api/reflection/KnowledgeBaseServer/KnowledgeBaseServer/KnowledgeServer/AddSnippet", sBody, true);
 		}
 
+		public static string Query(string sTagList)
+		{
+			//string sResponse = WebCommunications.SendGetRequest("http://dwlapi.azurewebsites.net/api/reflection/KnowledgeBaseServer/KnowledgeBaseServer/KnowledgeServer/ConstructPage?squery=" + sTagList, true);
+			//string sResponse = WebCommunications.SendGetRequest("http://localhost:16651/api/reflection/KnowledgeBaseServer/KnowledgeBaseServer/KnowledgeServer/ConstructPage?squery=" + sTagList, true);
+			KnowledgeServer ks = new KnowledgeServer();
+			string sResponse = ks.ConstructPage("Genetic_Algorithm");
+			return sResponse;
+		}
+
 		public static string EncodeXML(string sXML) { return sXML.Replace("&", "&amp;").Replace("<", "&lt;").Replace(">", "&gt;").Replace("\"", "&quot;").Replace("'", "&apos;"); }
 
 		public static void AddTestingSnippets()
 		{
-			AddSnippet("Definition,AI,Theory,Genetic_Algorithm,Wikipedia,Machine_Learning", "<snippet><p><b>Genetic Algorithm</b> - A genetic algorithm is a metaheuristic inspired by the process of natural selection, commonly used to find solutions to optimization and search problems using mutation, crossover, and selection.</p></snippet><source tag='Wikipedia'>https://en.wikipedia.org/wiki/Genetic_algorithm</source>");
-			AddSnippet("Definition,Genetic_Algorithm,Wikipedia,Chromosome,Genotype", "<snippet><p>Chromosome/Genotype - Set of parameters which define a proposed solution to the problem that the genetic algorithm is trying to solve. Often a binary representation (binary string) or a string representation.</p></snippet><source tag='Wikipedia'>https://en.wikipedia.org/wiki/Chromosome_(genetic_algorithm)</source>");
+			AddSnippet("Definition,AI,Theory,Genetic_Algorithm,Wikipedia,Machine_Learning,Genetic_Algorithm_Definition", "<snippet><p><b>Genetic Algorithm</b> - A genetic algorithm is a metaheuristic inspired by the process of natural selection, commonly used to find solutions to optimization and search problems using mutation, crossover, and selection.</p></snippet><source tag='Wikipedia'>https://en.wikipedia.org/wiki/Genetic_algorithm</source>");
+			AddSnippet("Definition,Genetic_Algorithm,Wikipedia,Chromosome,Genotype,Chromosome_Definition", "<snippet><p>Chromosome/Genotype - Set of parameters which define a proposed solution to the problem that the genetic algorithm is trying to solve. Often a binary representation (binary string) or a string representation.</p></snippet><source tag='Wikipedia'>https://en.wikipedia.org/wiki/Chromosome_(genetic_algorithm)</source>");
 			AddSnippet("Note,Genetic_Algorithm,Wikipedia", "<snippet><p>\"GA's cannot effectively solve problems in which the only fitness measure is a single right/wrong measure (like decision problems), as there is no way to converge on the solution. However, if the situation allows the success/failure trial to be repeated giving different results, then the ratio of successes to failures provides a suitable fitness measure.\"</p></snippet><source tag='Wikipedia'>https://en.wikipedia.org/wiki/Genetic_algorithm</source>");
 			AddSnippet("Depth,Implementation,Genetic_Algorithm,Obitko", "<snippet><ol><li>Start - Generate random population of n chromosomes (suitable solutions to problem)</li><li>Fitness - Evaluate the fitness f(x) of each chromosome x in the population</li><li>New population - Create a new population by repeating the following steps until the new population is complete<ol><li>Selection - Select two parent chromosomes from a population according to their fitness (the better fitness, the bigger chance to be selected)</li><li>Crossover - With a crossover probability cross over the parents to form a new offspring (children). If no crossover was performed, offspring is an exact copy of parents.</li><li>Mutation - With a mutation probability mutate new offspring at each locus (position in chromosome)</li><li>Accepting - Place new offspring in a population</li></ol></li><li>Replace - Use new generated population for a further run of algorithm</li><li>Test - If the end condition is satisfied, stop, and return the best soultion in current population.</li><li>Loop - Go to step 2</li></ol></snippet><source tag='Obitko'>http://www.obitko.com/tutorials/genetic-algorithms/ga-basic-description.php</source>");
 			AddSnippet("Depth,Implementation,Note,Genetic_Algorithm,Initial,Franklin", "<snippet><p>Generating initial population: \"First forms an initial population by randomly choosing values (alleles) for each of the features (genes)\"</snippet><source tag='Franklin'>Franklin 177</source>");
-			AddSnippet("Definition,Theory,AI,Church-Turing_Thesis,Franklin", "<snippet><p>Church-Turing Thesis - \"Any effective procedure (algorithm) can be implemented via a Turing machine.\"</p></snippet><source tag='Franklin'>Franklin 79</source>");
+			AddSnippet("Theory,AI,Church-Turing_Thesis,Franklin,Church-Turing_Thesis_Theory", "<snippet><p>Church-Turing Thesis - \"Any effective procedure (algorithm) can be implemented via a Turing machine.\"</p></snippet><source tag='Franklin'>Franklin 79</source>");
 		}
 	}
 }
